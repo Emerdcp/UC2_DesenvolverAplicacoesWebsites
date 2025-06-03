7735540371ef53e8e5b7793f2ec354d9a116dd47
Suportes <suporte@vlcorrea.com.br>
2025-06-03 17:03:25 -0300

    Acerto das páginas de blog e produtos
    Acerto das páginas de blog e produto

--- 2025.05.05_SiteLoja/scripts.js
+++ 2025.05.05_SiteLoja/scripts.js
@@ -1,36 +1,66 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Função para inicializar um slideshow
-    function initSlideshow(slideshowId) {
-      let slideIndex = 0;
-      const slides = document.querySelectorAll(`#${slideshowId} .slideP`);
-  
-      function showSlide(index) {
-        slides.forEach(slide => slide.classList.remove('active'));
-        slides[index].classList.add('active');
-      }
-  
-      function nextSlide() {
-        slideIndex = (slideIndex + 1) % slides.length;
-        showSlide(slideIndex);
-      }
-  
-      showSlide(slideIndex);
-      setInterval(nextSlide, 3000); // Muda a cada 3 segundos
+  // Função para inicializar um slideshow
+  function initSlideshow(slideshowId) {
+    let slideIndex = 0;
+    const slides = document.querySelectorAll(`#${slideshowId} .slideP`);
+
+    function showSlide(index) {
+      slides.forEach(slide => slide.classList.remove('active'));
+      slides[index].classList.add('active');
     }
-  
-    // Inicializa cada slideshow individualmente
-    initSlideshow('slideshow1');
-    initSlideshow('slideshow2');
-    initSlideshow('slideshow3');
-  });
-  
+
+    function nextSlide() {
+      slideIndex = (slideIndex + 1) % slides.length;
+      showSlide(slideIndex);
+    }
+
+    showSlide(slideIndex);
+    setInterval(nextSlide, 3000); // Muda a cada 3 segundos
+  }
+
+  // Inicializa cada slideshow individualmente
+  initSlideshow('slideshow1');
+  initSlideshow('slideshow2');
+  initSlideshow('slideshow3');
+});
+
 //=============== Script para rolagem ===================
 
 function scrollCards(value) {
-    const container = document.getElementById("scroll-container");
-    container.scrollLeft += value;
+  const container = document.getElementById("scroll-container");
+  container.scrollLeft += value;
 }
 
 
+//=============== Script para Slide Parceirias ===================
+
+document.addEventListener('DOMContentLoaded', function () {
+  function initSlideshow(slideshowId) {
+    let slideIndex = 0;
+    const slides = document.querySelectorAll(`#${slideshowId} .slide`);
+
+    function showSlide(index) {
+      slides.forEach(slide => slide.style.display = "none");
+      slides[index].style.display = "flex";
+    }
+
+    function nextSlide() {
+      slideIndex = (slideIndex + 1) % slides.length;
+      showSlide(slideIndex);
+    }
+
+    showSlide(slideIndex);
+    setInterval(nextSlide, 3000);
+  }
+
+  initSlideshow('slideshow1');
+  initSlideshow('slideshow2');
+  initSlideshow('slideshow3');
+});
+
+function scrollCards(value) {
+  const container = document.getElementById("scroll-container");
+  container.scrollLeft += value;
+}
 
 
